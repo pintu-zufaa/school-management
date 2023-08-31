008a434e9c080ee0a07fab507c8e0d117ee1af72
PintuRoy121 <pintu@zufaa.com>
2023-08-31 15:57:18 +0600

    banner fixed

--- src/app/Components/Header/Header.jsx
+++ src/app/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
-import banner from '../../../../assets/Images/Rectangle 3.png'
-import name from '../../../../assets/Images/Rajshahi Collegiate School.png'
+import banner from '../../../../assets/Images/Rectangle3.png'
+import name from '../../../../assets/Images/RajshahiCollegiateSchool.png'
 import Image from 'next/image';
 
 const Header = () => {
@@ -9,7 +9,6 @@ const Header = () => {
         <div className='w-full relative'>
             <Image src={banner} alt='' />
             <Image src={name} alt='' className='absolute right-10 bottom-7 w-2/5' />
-
         </div>
     );
 };
