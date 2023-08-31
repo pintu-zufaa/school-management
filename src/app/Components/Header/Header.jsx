fbe78d87391937e539e5414da558e01e80585fd3
PintuRoy121 <pintu@zufaa.com>
2023-08-31 16:03:34 +0600

    banner fixed

--- src/app/Components/Header/Header.jsx
+++ src/app/Components/Header/Header.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
-import banner from '../../../../assets/Images/Rectangle3.png'
+import banner from '../../../../assets/Images/schoolbanner.png'
 import name from '../../../../assets/Images/RajshahiCollegiateSchool.png'
 import Image from 'next/image';
 
 const Header = () => {
     return (
         <div className='w-full relative'>
-            <Image src={banner} alt='' />
+            <Image src={banner} alt='' className='w-full' />
             <Image src={name} alt='' className='absolute right-10 bottom-7 w-2/5' />
         </div>
     );
